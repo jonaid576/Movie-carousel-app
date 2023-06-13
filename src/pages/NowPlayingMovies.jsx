b33067b9ffb625e16279257e41916329fcee4cba
chaser945 <113222684+chaser945@users.noreply.github.com>
2023-06-13 18:15:26 +0500

    fixed scroll restoration

--- src/pages/NowPlayingMovies.jsx
+++ src/pages/NowPlayingMovies.jsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom"
 import useFetch from "../useFetch"
 import LoadingSpin from "../components/LoadingSpin"
 import Error from "../components/Error"
+import { MovieCard } from "../components/MovieCard"
 
 export default function NowPlayingMovies() {
   const [loading, error, data] = useFetch(
@@ -21,21 +21,8 @@ export default function NowPlayingMovies() {
       <h1 className="now-playing-h1">Trending Movies</h1>
       <div className="now-playing-movies-wrapper">
         {data.results.map((movie) => {
-          return (
-            <div className="now-playing-movie-card" key={movie.id}>
-              <Link to={`${movie.id}`} state={movie}>
-                <img
-                  className="now-playing-img"
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt={movie.title}
-                ></img>
-                <h3 className="now-playing-h3">{movie.title}</h3>
-                <p className="now-playing-p">
-                  ({movie.release_date.split("-")[0]})
-                </p>
-              </Link>
-            </div>
-          )
+          const { id } = movie
+          return <MovieCard key={id} movie={movie} />
         })}
       </div>
     </section>
